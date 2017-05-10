1d7deb17a6bae702418ebc39725cf6d2ab993ae6
brianbier <briansbier@gmail.com>
2017-05-10 17:57:22 -0400

    Routes and passing params

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <router-link to="/">Home</router-link>
+    <router-link to="/message/10">Message</router-link>
+    <router-link to="/message/434343">Message</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/components/Message.vue
+++ src/components/Message.vue
@@ -2,6 +2,8 @@
   <div>
     <h1>{{ message }}</h1>
     <app-input :msg="message" @messageChange="message = $event"></app-input>
+    <p>Message ID {{ $route.params.messageId }}</p>
+    <button @click="goHome">Go Home</button>
   </div>
 
 </template>
@@ -9,15 +11,28 @@
 
 <script>
 import Input from './Input'
-
 export default {
+  watch: {
+    '$route' (to, from) {
+      alert(to.params.messageId)
+    }
+  },
+  created () {
+    alert(this.$route.params.messageId)
+  },
   data () {
     return {
       message: 'This is a great Message'
+
     }
   },
   components: {
     'app-input': Input
+  },
+  methods: {
+    goHome () {
+      this.$router.push('/')
+    }
   }
 }
 </script>
